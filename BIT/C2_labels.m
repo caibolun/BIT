81dcd32e99d82063165aafb600c06f5d6c80eedc
caibolun <caibolun@gmail.com>
2016-03-01 20:06:23 +0800

    Update C2_labels.m

--- BIT/C2_labels.m
+++ BIT/C2_labels.m
@@ -1,6 +1,6 @@
 function labels = C2_labels(sigma, sz)
     labels=[];
-	[rs, cs] = ndgrid((1:sz(1)) - floor(sz(1)/2), (1:sz(2)) - floor(sz(2)/2));
+    [rs, cs] = ndgrid((1:sz(1)) - floor(sz(1)/2), (1:sz(2)) - floor(sz(2)/2));
     for item=1:numel(sigma)
         label = exp(-0.5 / sigma(item)^2 * (rs.^2 + cs.^2));
         label(floor(sz(1)/2),floor(sz(2)/2)) = 1;
